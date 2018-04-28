24fbee69aa8a4e4e21216410fef7486d1149fd29
WeekFax <we9697@mail.ru>
2018-04-28 15:17:46 +0700

    ver 0.2
    Change thresh filtering

--- source.py
+++ source.py
@@ -83,139 +83,144 @@ DIGITS_LOOKUP = {
 }
 cap = cv2.VideoCapture(0)
 while True:
+
     # load the example image
     ret, image = cap.read()
-    #image = cv2.imread("photo.jpg")
-    try:
-        # pre-process the image by resizing it, converting it to
-        # graycale, blurring it, and computing an edge map
-        image = imutils.resize(image, height=500)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (3, 3),1)
-        edged = cv2.Canny(blurred, 100, 200,255)
+    #image = cv2.imread("photo0.jpg")
+    (X, Y, W, H) = (36, 26, 22, 36)
 
-        cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-        displayCnt = None
-        X=0
-        Y=0
+    # pre-process the image by resizing it, converting it to
+    # graycale, blurring it, and computing an edge map
+    image = imutils.resize(image, height=500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5),1)
+    edged = cv2.Canny(gray, 100, 200,5)
 
-        # loop over the contours
-        for c in cnts:
-            # approximate the contour
-            peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    displayCnt = None
 
-            # if the contour has four vertices, then we have found
-            # the thermostat display
-            if len(approx) == 4:
-                displayCnt = approx.reshape(4, 2)
-                X,Y=displayCnt[0]
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+        # if the contour has four vertices, then we have found
+        # the thermostat display
+        if len(approx) == 4:
+            (x, y, w, h) = cv2.boundingRect(approx)
+            if (abs((w / h) - 1.7) < 0.2)and(h > 50):
                 cv2.drawContours(image, [approx], -1, (255, 0, 0), 1)
-
+                #cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 1)
+                displayCnt = approx.reshape(4, 2)
                 break
 
-        warped = four_point_transform(gray, displayCnt)
+    if(displayCnt is not None):
+
+        warped = four_point_transform(blurred, displayCnt)
         output = four_point_transform(image, displayCnt)
+
 
         warped = imutils.resize(warped, height=80)
         output = imutils.resize(output, height=80)
-        thresh = cv2.threshold(warped, 0, 255,
-            cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (1, 1))
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+        thr=cv2.adaptiveThreshold(warped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,21, 3)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+        thr = cv2.morphologyEx(thr, cv2.MORPH_CLOSE, kernel)
 
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE,
-                                cv2.CHAIN_APPROX_SIMPLE)
+
+        #cv2.rectangle(output, (x, y), (x+w, y+h), (0, 0, 255), 1)
+
+        cnts = cv2.findContours(thr.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
         digitCnts = []
-        #cv2.drawContours(output,cnts,-1,(255,0,0))
-        # loop over the digit area candidates
+
+        #cv2.drawContours(output, cnts, -1, (255, 0, 0), 1)
 
         for c in cnts:
-            # compute the bounding box of the contour
-            (x, y, w, h) = cv2.boundingRect(c)
-            #cv2.rectangle(output,(x,y),(x+w, y+h),(255,0,0),1)
-            # if the contour is sufficiently large, it must be a digit
-            if w >= 15 and (h >= 30 and h <= 40):
-                cv2.rectangle(image, (X+x, Y+y), (X+x + w, Y+y + h), (0, 0, 255), 1)
-                digitCnts.append(c)
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+            (x, y, w, h) = cv2.boundingRect(approx)
+            if((w-W)<5 and abs(h-H)<7):
+                x=(x+w)-W
+                y=(y+h)-H
+
+                digitCnts.append(approx)
 
 
+        try:
+            digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
+            digits = []
 
-        digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
-        digits = []
+            for c in digitCnts:
+                # extract the digit ROI
+                (x, y, w, h) = cv2.boundingRect(c)
+                x = (x + w) - W
+                y = (y + h) - H +2
+                h = H-1
+                w = W
+                roi = thr[y:y + h, x:x + w]
+                #cv2.rectangle(output, (x, y), (x + W, y + H), (0, 255, 0), 1)
 
-        for c in digitCnts:
-            # extract the digit ROI
-            (x, y, w, h) = cv2.boundingRect(c)
-            roi = thresh[y:y + h, x:x + w]
+                # compute the width and height of each of the 7 segments
+                # we are going to examine
+                (roiH, roiW) = roi.shape
+                (dW, dH) = (int(roiW * 0.20), int(roiH * 0.15))
+                dHC = int(roiH * 0.05)
 
-            # compute the width and height of each of the 7 segments
-            # we are going to examine
-            (roiH, roiW) = roi.shape
-            (dW, dH) = (int(roiW * 0.35), int(roiH * 0.2))
-            dHC = int(roiH * 0.05)
-
-            # define the set of 7 segments
-            segments = [
-                ((0, 0), (w, dH)),  # top
-                ((0, 0), (dW, h // 2)),  # top-left
-                ((w - dW, 0), (w, h // 2)),  # top-right
-                ((0, (h // 2) - dHC), (w-2, (h // 2) + dHC)),  # center
-                ((-3, h // 2), (dW-3, h)),  # bottom-left
-                ((w - dW-3, h // 2), (w-3, h)),  # bottom-right
-                ((0, h - dH), (w-5, h))  # bottom
-            ]
-            on = [0] * len(segments)
-
-            for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
-                # extract the segment ROI, count the total number of
-                # thresholded pixels in the segment, and then compute
-                # the area of the segment
-                segROI = roi[yA:yB, xA:xB]
-                total = cv2.countNonZero(segROI)
-                area = (xB - xA) * (yB - yA)
-
-                # if the total number of non-zero pixels is greater than
-                # 50% of the area, mark the segment as "on"
-                if total / float(area) > 0.5:
-                    on[i] = 1
-                    #cv2.rectangle(output, (x+xA, y+yA), (x+xB, y+yB), (0, 0, 255), 1)
-
-            for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
-                # extract the segment ROI, count the total number of
-                # thresholded pixels in the segment, and then compute
-                # the area of the segment
-                segROI = roi[yA:yB, xA:xB]
-                total = cv2.countNonZero(segROI)
-                area = (xB - xA) * (yB - yA)
-
-                # if the total number of non-zero pixels is greater than
-                # 50% of the area, mark the segment as "on"
-                if total / float(area) > 0.5:
-                    on[i] = 1
-
-                # lookup the digit and draw it on the image
-            digit = DIGITS_LOOKUP[tuple(on)]
-            digits.append(digit)
-            cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
-            cv2.putText(output, str(digit), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
-        if len(digitCnts)==1:
-            print(u"{} \u00b0C".format(*digits))
-        if len(digitCnts)==2:
-            print(u"{}{} \u00b0C".format(*digits))
+                # define the set of 7 segments
+                segments = [
+                    ((4, 0), (w-2, dH)),  # top
+                    ((2, 2), (dW + 2, h // 2)),  # top-left
+                    ((w - dW-3, 2), (w-3, h // 2+2)),  # top-right
+                    ((3, (h // 2) - dHC), (w-3, (h // 2) + dHC)),  # center
+                    ((1, h // 2), (dW, h)),  # bottom-left
+                    ((w - dW-4 , h // 2), (w-4, h)),  # bottom-right
+                    ((0, h - dH), (w-4, h))  # bottom
+                ]
+                on = [0] * len(segments)
 
 
+                for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
+                    # extract the segment ROI, count the total number of
+                    # thresholded pixels in the segment, and then compute
+                    # the area of the segment
+                    segROI = roi[yA:yB, xA:xB]
+                    total = cv2.countNonZero(segROI)
+                    area = (xB - xA) * (yB - yA)
 
-        #output = imutils.resize(output, height=500)
-        cv2.imshow('result', image)
-    except:
-        cv2.imshow('result',image)
 
+                    # if the total number of non-zero pixels is greater than
+                    # 50% of the area, mark the segment as "on"
+                    if total / float(area) > 0.5:
+                        on[i] = 1
+                        cv2.rectangle(output, (xA + x, yA + y), (xB + x, yB + y), (255, 0, 0), 1)
+                    else:
+                        cv2.rectangle(output, (xA + x, yA + y), (xB + x, yB + y), (255, 0, 255), 1)
+                        pass
+
+                    # lookup the digit and draw it on the image
+
+                digit = DIGITS_LOOKUP[tuple(on)]
+                digits.append(digit)
+                #cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
+                #cv2.putText(output, str(digit), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+
+            print(digits)
+
+        except:
+            print("err")
+
+
+        cv2.imshow('out', output)
+
+    cv2.imshow('result', image)
+
+    #while True:
+        #cv2.imshow('result', thr)
     ch = cv2.waitKey(5)
     if ch == 27:
         break
-
 cv2.destroyAllWindows()
